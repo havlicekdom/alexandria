3eab0ac83c38b6a6b773ea5ebbd1e8cfeef33b12
Dominik Havlicek <dominik.havlicek01@currys.co.uk>
2022-10-13 09:33:42 +0200

    feat: move authors detail to redux

--- packages/client/src/store/authors/authorsSlice.ts
+++ packages/client/src/store/authors/authorsSlice.ts
@@ -2,14 +2,16 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosError } from 'axios';
 import { RootState } from 'store/store';
 import { Author } from 'types/author';
-import { fetchAuthorsList } from './authorsAPI';
+import { fetchAuthorsDetail, fetchAuthorsList } from './authorsAPI';
 
 export interface AuthorsState {
   list: Author[];
+  detail: Author | null;
 }
 
 const initialState: AuthorsState = {
   list: [],
+  detail: null,
 };
 
 export const getAuthorsList = createAsyncThunk(
@@ -34,17 +36,44 @@ export const getAuthorsList = createAsyncThunk(
   },
 );
 
+export const getAuthorsDetail = createAsyncThunk(
+  'authors/getDetail',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      const response = await fetchAuthorsDetail(id);
+
+      return {
+        detail: response.data,
+        message: null,
+      };
+    } catch (error) {
+      const { response } = error as AxiosError;
+
+      if (!response) {
+        throw error;
+      }
+
+      return rejectWithValue(response.data);
+    }
+  },
+);
+
 export const authorsSlice = createSlice({
   name: 'authors',
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getAuthorsList.fulfilled, (state, action) => {
-      state.list = action.payload.authors;
-    });
+    builder
+      .addCase(getAuthorsList.fulfilled, (state, action) => {
+        state.list = action.payload.authors;
+      })
+      .addCase(getAuthorsDetail.fulfilled, (state, action) => {
+        state.detail = action.payload.detail;
+      });
   },
 });
 
 export const selectAuthorsList = (state: RootState) => state.authors.list;
+export const selectAuthorsDetail = (state: RootState) => state.authors.detail;
 
 export default authorsSlice.reducer;
